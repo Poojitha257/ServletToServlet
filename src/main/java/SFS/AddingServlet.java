7350b7d2676adfa16d00b96d6fa3b01438754b2c
Poojitha257 <gpoojitha357@gmail.com>
2024-12-02 22:02:46 +0530

    updated

--- src/main/java/SFS/AddingServlet.java
+++ src/main/java/SFS/AddingServlet.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/add") 
 public class AddingServlet extends HttpServlet {
@@ -15,8 +17,22 @@ public class AddingServlet extends HttpServlet {
 		int j=Integer.parseInt(req.getParameter("num2"));
 		int k=i+j;
 		
-		resp.sendRedirect("sq?k="+k);  // this is url Redirecting
 		
+		//********* cookies *********//
+		Cookie cookie = new Cookie("k", k + "");
+		resp.addCookie(cookie);
+		resp.sendRedirect("sq");
+		
+		
+		
+		//*********  session management ******//
+//		HttpSession session =req.getSession();
+//		session.setAttribute("k", k);
+//		resp.sendRedirect("sq");
+		
+		//***** send redirect******//
+//		resp.sendRedirect("sq?k="+k);  // this is url Redirecting
+		//********Request dispatcher*****//
 //		req.setAttribute("k", k);
 //	req.getRequestDispatcher("sq").forward(req, resp);;
 
--- src/main/java/SFS/SqServlet.java
+++ src/main/java/SFS/SqServlet.java
@@ -4,19 +4,50 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/sq")
 public class SqServlet  extends HttpServlet{
 @Override
 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//	int k=(int)req.getAttribute("k");
+//*********** cookies *********//
+	int k=0;
+Cookie cookies[]= req.getCookies();
+for (Cookie c : cookies) {
+	if(c.getName().equals("k"))
+	{
+		k=Integer.parseInt(c.getValue());
+	}
+}
+	k=k*k;
+	resp.getWriter().print("Result is " + k);
+System.out.println("sq called");
+	
+	
+	
+	//*********	session management *********//
+	
+//	HttpSession session=req.getSession();
+//	int k=(int)session.getAttribute("k");
+//	k=k*k;
+//	resp.getWriter().print("Result is " + k);
+//	System.out.println("sq called");
+	
+	//******send redirect****//
+	
+//	int k=Integer.parseInt(req.getParameter("k"));
+//	resp.getWriter().print("sq is calling and Result is " + k);
+	
+	//******** Request dispatcher******//
+	
+	//int k=(int)req.getAttribute("k");
 //	k=k*k;
 //	resp.getWriter().print("calling servlet from  another servlet " + " Result is " + k);
 
-int k=Integer.parseInt(req.getParameter("k"));
-resp.getWriter().print("sq is calling and Result is " + k);
+
 }
 }
